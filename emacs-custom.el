6891240421c87e2475ba20873ddc52c795492b88
psdp <pedro@dellapiazza.com>
2025-12-20 05:52:29 +0000

    Changed a bunch of things since last time, will be more attentive

--- emacs-custom.el
+++ emacs-custom.el
@@ -4,10 +4,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/programming/org/agenda.org"))
+ '(org-agenda-files '("/home/psdp/programming/org/agenda.org"))
  '(package-selected-packages
-   '(arduino-cli-mode arduino-mode auctex cdlatex elfeed fzf magit
-                      markdown-mode multiple-cursors pdf-tools smex
+   '(arduino-cli-mode arduino-mode auctex buffer-move cdlatex corfu
+                      elfeed fzf go-mode magit marginalia
+                      markdown-mode multiple-cursors pdf-tools vertico
                       yasnippet))
  '(tex-default-mode 'LaTeX-mode))
 (custom-set-faces
--- init.el
+++ init.el
@@ -32,7 +32,7 @@
 (scroll-bar-mode -1)
 (global-visual-line-mode t)
 (global-display-line-numbers-mode 1)
-(load-theme 'modus-vivendi)
+(load-theme 'modus-operandi)
 (global-hl-line-mode 1)
 (show-paren-mode 1)
 (column-number-mode 1)
@@ -41,44 +41,83 @@
 (add-to-list 'default-frame-alist
              '(font . "DejaVu Sans Mono-12"))
 
+;;dired changes
+
+(setq dired-open-extensions '(("mkv" . "mpv")))
+
 ;;electric parenthesis mode
 (electric-pair-mode 0)
 
 ;;change buffer menu for ibuffer
 (global-set-key (kbd "C-x C-b") #'ibuffer)
 
+;;cc mode changes (indentation in later section)
+
+(defun my-make-CR-do-indent ()
+    (define-key c-mode-base-map "\C-m" 'c-context-line-break))
+(add-hook 'c-initialization-hook 'my-make-CR-do-indent)
+
 ;;indentation changes
 (setq-default indent-tabs-mode nil)
 (electric-indent-mode 1)
 (setq c-basic-offset 4)
 (setq lisp-body-indent 4)
+(setq tab-width 4)
 ;;column changes
 (setq-default set-fill-column 80)
+
+;;dict mode changes
+(setq dictionary-server "dict.org")
+
 ;;ido and fido mode
-(ido-mode 1)
-(setq ido-auto-merge-delay-time 999999999)
+;;(ido-mode 1)
+;;(setq ido-auto-merge-delay-time 999999999)
 ;;(fido-mode 1) 
-;;fido mode is really slow so I am using smex instead until they fix this shit
-(use-package smex
+;;fido mode is really slow so I am using vertico and marginalia instead until they fix this shit and make it less slow
+(use-package vertico
     :ensure t
-    :bind
-    ("M-x" . smex))
+    :config
+    (vertico-mode 1))
+(use-package marginalia
+    :ensure t
+    :config
+    (marginalia-mode 1))
 
 ;;org mode changes
 (setq org-directory "~/programming/org")
 (add-hook 'org-mode-hook 'visual-line-mode)
 (add-hook 'org-mode-hook 'flyspell-mode)
 (setq org-image-actual-width 600)
+;;some keybind changes, tab was being weird, there was no heading with control
+;;enter and the buffer movements were being overwritten by org mode keybinds
+;;even though they were global
 (with-eval-after-load "org"
     (define-key org-mode-map (kbd "<tab>") #'org-cycle)
-    (define-key org-mode-map (kbd "C-<return>") #'org-insert-heading))
+    (define-key org-mode-map (kbd "C-<return>") #'org-insert-heading)
+    (define-key org-mode-map (kbd "<C-S-up>")     'buf-move-up)    
+    (define-key org-mode-map (kbd "<C-S-down>")   'buf-move-down)  
+    (define-key org-mode-map (kbd "<C-S-left>")   'buf-move-left)  
+    (define-key org-mode-map (kbd "<C-S-right>")  'buf-move-right))
 
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "PROJECT(p)" "READING(r)" "|" "DONE(d)" "CANCELLED(c)")))
+;;changes for org agenda
+(setq org-log-done t)
+(setq org-agenda-files '("~/programming/org/agendas"))
+(global-set-key (kbd "C-c a") 'org-agenda)
 ;;org babel changes
 (org-babel-do-load-languages 'org-babel-load-languages
 			     '(
 			       (python     . t)
 			       (C       . t)
                                ))
+
+;;org capture templates
+(global-set-key (kbd "C-c c") 'org-capture)
+(setq org-capture-templates
+      '(("i" "Inbox" entry
+         (file "~/programming/org/inbox.org")
+         "* TODO %?")))
 
 ;;shell changes
 (with-eval-after-load "shell"
@@ -116,12 +155,9 @@
     :config
     (setq TeX-auto-save t)
     (setq TeX-parse-self t)
-    (setq-default TeX-master nil)
-
     (add-hook 'LaTeX-mode-hook 'visual-line-mode)
     (add-hook 'LaTeX-mode-hook 'flyspell-mode)
     (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-
     (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
     (setq reftex-plug-into-AUCTeX t))
 
@@ -142,7 +178,7 @@
 (use-package magit
     :ensure t
     :bind
-    ("C-x C-g" . magit-status)
+    ("C-x g" . magit-status)
     :config)
 
 ;;erc
@@ -162,20 +198,23 @@
     :ensure t
     :config
     (setq elfeed-feeds
-          '("https://stonetoss.com/comic/feed/"
-            "https://analognowhere.com/feed/rss.xml"
-            "https://archlinux.org/feeds/news/"
-            "https://inv.nadeko.net/feed/channel/UCD6VugMZKRhSyzWEWA9W2fg" ;;sseth
+          '("https://inv.nadeko.net/feed/channel/UCD6VugMZKRhSyzWEWA9W2fg" ;;sseth
             "https://inv.nadeko.net/feed/channel/UCyEJzdViuBHCo4Cokj4-BgQ" ;;picothespicywarlord
             "https://inv.nadeko.net/feed/channel/UCP399QC1HNG2CtpzvmelWEg" ;;pacaranaabelcirilo
             "https://inv.nadeko.net/feed/channel/UC2eYFnH61tmytImy1mTYvhA" ;;luke smith
+            "https://inv.nadeko.net/feed/channel/UCfwJBTwTgdCj5IHmBcOD8Vg" ;;shreddednerd
+            "https://inv.nadeko.net/feed/channel/UCb_sF2m3-2azOqeNEdMwQPw" ;;matthewmatosis
+            "https://yewtu.be/feed/channel/UCYSDUtXMbJ-kLe0s5XpxUng"       ;;more hawka
+            "https://yewtu.be/feed/channel/UCEq0icugzGTAw_q0LaDFmZw"       ;;noob2dev
             "https://battlepenguin.com/feed.xml"
+            "https://joshblais.com/index.xml"
             "https://www.eff.org/rss/updates.xml"
             "https://act.eff.org/action.atom"
             "https://sadgrl.online/posts/rss.xml"
             "https://corbettreport.com/feed/"
-            "https://inv.nadeko.net/feed/channel/UCfwJBTwTgdCj5IHmBcOD8Vg" ;;shreddednerd
-            "https://inv.nadeko.net/feed/channel/UCb_sF2m3-2azOqeNEdMwQPw" ;;matthewmatosis
+            "https://stonetoss.com/comic/feed/"
+            "https://analognowhere.com/feed/rss.xml"
+            "https://archlinux.org/feeds/news/"
             )))
 
 ;;fzf
@@ -196,3 +235,36 @@
         ;; If nil, the fzf buffer will appear at the top of the window
         fzf/position-bottom t
         fzf/window-height 15))
+
+;;go mode
+(use-package go-mode
+    :ensure t
+    :config
+    )
+
+;;buffer move
+(use-package buffer-move
+    :ensure t
+    :config
+    (global-set-key (kbd "<C-S-up>")     'buf-move-up)
+    (global-set-key (kbd "<C-S-down>")   'buf-move-down)
+    (global-set-key (kbd "<C-S-left>")   'buf-move-left)
+    (global-set-key (kbd "<C-S-right>")  'buf-move-right))
+
+;;corfu (completion framework)
+(use-package corfu
+    :ensure t
+    :custom
+    (corfu-cycle t)
+    (corfu-auto t)
+    (corfu-auto-prefix 2)
+    (corfu-auto-delay 0.0)
+    :bind (:map corfu-map
+                ("M-SPC" . corfu-insert-separator)
+                ("RET" . nil)
+                ("TAB" . corfu-next)
+                ("S-TAB" . corfu-previous)
+                ("S-RET" . corfu-insert))
+    :init
+    (global-corfu-mode)
+)
